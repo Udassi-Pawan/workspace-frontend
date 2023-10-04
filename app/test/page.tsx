2f2919383d254bf46ec72c7d213b51f7133a7ee9
Pawan Udassi <pawanudassi@hotmail.com>
2023-10-04 20:33:52 +0530

    fixed bug

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,41 +1,6 @@
-"use client";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/shardcn/components/ui/alert-dialog";
-import { ReactNode, createContext, useState } from "react";
+import React from "react";
 
-const AlertContext = createContext<any>(null);
-
-export default function AlertProvider({ children }: { children: ReactNode }) {
-  const [dialogueText, setDialogueText] = useState<string>("");
-  return (
-    <AlertContext.Provider value={{ setDialogueText }}>
-      <div className="relative">
-        <div className="absolute">
-          <AlertDialog open={dialogueText != ""}>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Alert</AlertDialogTitle>
-                <AlertDialogDescription>{dialogueText}</AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogAction onClick={() => setDialogueText("")}>
-                  Continue
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
-        {children}
-      </div>
-    </AlertContext.Provider>
-  );
+export interface pageProps {}
+export default function page({}: pageProps) {
+  return <></>;
 }
-
-export { AlertContext };
